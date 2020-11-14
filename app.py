97470172eb837d4b85d03336ed8c6748b36a62cb
Ashwin Sampath <asampat2@uci.edu>
2020-11-14 13:39:42 -0800

    a little progress on app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-import os
-import util
-import db
-from flask import Flask, json
+from flask import Flask, json, request
 from flask_cors import CORS
 from flask_restful import Api
-from resources.user import User
+#from resources.user import User
 from dotenv import load_dotenv
+from credentials import id, key
 
 # Load Environment variables
 load_dotenv()
@@ -13,15 +11,7 @@ load_dotenv()
 app = Flask(__name__)
 # Allow cross domain apps to access API
 CORS(app)
-
-# Provide Mongo Atlas URI, stored in config file
-app.config["MONGO_URI"] = os.getenv("MONGO_URI_MASTER")
-# Set custom JSON Encoder for Mongo Object
-app.json_encoder = util.MongoEncoder
-db.mongo.init_app(app)
 api = Api(app)
-
-api.add_resource(User, "/user")
 
 
 # Vanilla Flask route
@@ -36,6 +26,35 @@ def index():
 def handle_error(err):
     messages = err.data.get("messages", ["Invalid request."])
     return json.jsonify({"errors": messages}), err.code
+
+
+@app.route("/search")
+def search():
+    #Base url to add to
+    URL = "https://api.edamam.com/search?"
+
+    #request.args is a dict that contains the search parameters
+    args = request.args
+
+    if len(args) != 0:
+        #adds query text to url
+        if 'q' in args:
+            URL += 'q=' + args['q'] + '&'
+        else:
+            return "invalid search"
+        
+        #Adds id and key to request so it can be authenticated by the Edamam API
+        URL += "app_id=" + id + '&'
+        URL += "app_key=" + key
+
+        response = requests.get(URL).json()
+
+        #Not sure if I just return the response or do something else    
+
+        
+    else:
+        #Also not sure if returning just this is ok or if something else is needed
+        return "Empty search"
 
 
 if __name__ == "__main__":
